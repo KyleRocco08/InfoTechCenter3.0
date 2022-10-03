d32f6c46e294a3070fa580ffed8e67840802b7f9
KROCCO <kylerocco08@gmail.com>
2022-10-03 08:07:18 -0400

    Add Print Statements to Welcome Visitors

--- OperationFury.py
+++ OperationFury.py
@@ -6,3 +6,7 @@
 Our Welcome Screen will Start our program letting
 drivers know that the InfoTechCenter OS is Loading
 """
+
+print("\n\nWelcome to Operation Fury InfoTech Center")
+
+print("\nOperation Fury's Operating System Booting Up")
