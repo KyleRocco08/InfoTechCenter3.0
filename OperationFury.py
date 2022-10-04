29c08ebf92389cf4576e57757ebdeaaeacdcbec6
KROCCO <kylerocco08@gmail.com>
2022-10-04 08:36:54 -0400

    Add Color Text to Program

--- OperationFury.py
+++ OperationFury.py
@@ -10,7 +10,8 @@ drivers know that the InfoTechCenter OS is Loading
 #Import Libraries Here
 from time import sleep #We imported the Sleep function from the Time Library
 
+#("\033[3;31;40m")The \033 is the escape code. You can change 3 for different text styles, you can change 34 to change the color and change 40 for different backgroud colors
 
-print("\n\nWelcome to Operation Fury InfoTech Center")
+print("\033[3;31m Welcome to Operation Fury InfoTech Center")
 sleep(2)
-print("\nOperation Fury's Operating System Booting Up")
+print("\033[3;32m \nOperation Fury's Operating System is Booting Up")
