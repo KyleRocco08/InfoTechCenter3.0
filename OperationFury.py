58411eb4af7eb6f7f7752491640b00c7e95c8c43
KROCCO <kylerocco08@gmail.com>
2022-10-05 08:21:48 -0400

    Final OS Booting Up

--- OperationFury.py
+++ OperationFury.py
@@ -15,10 +15,11 @@ from time import sleep #We imported the Sleep function from the Time Library
 print("\033[3;31m Welcome to Operation Fury InfoTech Center")
 sleep(2)
 
-print("\033[3;32m \nOperation Fury's Operating System is Booting Up")
+print("\033[3;34m \nOperation Fury's Operating System is Booting Up")
 
 sleep(1.5)
 
 for i in range(8):
     print("\033[1;36m OS Booting Up")
     sleep(1)
+
