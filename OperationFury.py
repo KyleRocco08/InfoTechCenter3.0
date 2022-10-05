26225287d6bdb8a31d3f55977f193b199d6f0607
KROCCO <kylerocco08@gmail.com>
2022-10-05 08:39:06 -0400

    Initial Weather Commit

---
+++ OperationFury.py
@@ -0,0 +1,10 @@
+#Weather
+#Developer: Kyle Rocco
+#Version 1.0
+
+"""
+Create a function for our current weather using the
+ranodm.choice function to determine what the weather is
+picking from a list - using If, Elif and Else statements
+to check the condition and print a specific print line.
+"""
