ee548636a050f2e158971e2f5f73a37a1076ea12
KROCCO <kylerocco08@gmail.com>
2022-10-06 08:38:14 -0400

    Defined the Weather Function

--- OperationFury.py
+++ OperationFury.py
@@ -8,3 +8,17 @@ ranodm.choice function to determine what the weather is
 picking from a list - using If, Elif and Else statements
 to check the condition and print a specific print line.
 """
+
+# Import Libraries Here
+import random
+
+#Weather condition list using the random.choice library
+#to randomly choose a condition and stroing it in its brain
+def weather():
+    weatherForcast = ["Rain", "Snow", "Sunny", "Cloudy", "Foggy", "Storming", "Icy"]
+    weatherCondition = random.choice(weatherForcast)
+    return weatherCondition
+
+
+
+
