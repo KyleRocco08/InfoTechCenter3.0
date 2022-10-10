93c2d303115661f0d20de07657396af836693698
KROCCO <kylerocco08@gmail.com>
2022-10-10 07:58:16 -0400

    Added more elif and else statements checking weather conditions

--- OperationFury.py
+++ OperationFury.py
@@ -15,7 +15,7 @@ import random
 #Weather condition list using the random.choice library
 #to randomly choose a condition and stroing it in its brain
 def weather():
-    weatherForcast = ["Rain", "Snow", "Sunny", "Cloudy", "Foggy", "Storming", "Icy"]
+    weatherForcast = ["Rain", "Snow", "Sunny", "Windy", "Foggy", "Storming", "Icy"]
     weatherCondition = random.choice(weatherForcast)
     return weatherCondition
 
@@ -24,5 +24,23 @@ weatherAlert = weather()
 def vRS():
     if weatherAlert == "Icy":
         print("\nVRS has changed your alarm 30 minutes earlier based on the NWS forcast of",weatherAlert, "roads")
-
+        print("VRS will only allow your car to go 30MPH")
+    elif weatherAlert == "Snow":
+        print("\nVRS has changed your alarm 15 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 50MPH")
+    elif weatherAlert == "Rain":
+        print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 60MPH")
+    elif weatherAlert == "Windy":
+        print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 70MPH")
+    elif weatherAlert == "Foggy":
+        print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 60MPH")
+    elif weatherAlert == "Storming":
+        print("\nVRS has changed your alarm 15 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("VRS will only allow your car to go 50MPH")
+    else:
+        print("\nThe weather today is,",weatherAlert,"let's Gooooo!")
+        print("VRS will only allow your car to go 85MPH")
 vRS()
