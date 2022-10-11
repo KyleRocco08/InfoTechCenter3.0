45c144cb4cd216c21f0f3aee8c2bc223a6ecea39
KROCCO <kylerocco08@gmail.com>
2022-10-11 07:52:06 -0400

    Cleaning up program

--- OperationFury.py
+++ OperationFury.py
@@ -1,4 +1,12 @@
 
+#***********************************************************************************************************************
+#Import Libraries Here
+from time import sleep #We imported the Sleep function from the Time Library
+
+import random
+#***********************************************************************************************************************
+
+
 #Welcome Screen
 #Developer: Kyle Rocco
 #Version: 1.0
@@ -7,9 +15,6 @@
 Our Welcome Screen will Start our program letting
 drivers know that the InfoTechCenter OS is Loading
 """
-
-#Import Libraries Here
-from time import sleep #We imported the Sleep function from the Time Library
 
 #("\033[3;31;40m")The \033 is the escape code. You can change 3 for different text styles, you can change 34 to change the color and change 40 for different backgroud colors
 
@@ -36,8 +41,7 @@ picking from a list - using If, Elif and Else statements
 to check the condition and print a specific print line.
 """
 
-# Import Libraries Here
-import random
+
 
 #Weather condition list using the random.choice library
 #to randomly choose a condition and stroing it in its brain
@@ -50,25 +54,25 @@ weatherAlert = weather()
 
 def vRS():
     if weatherAlert == "Icy":
-        print("\nVRS has changed your alarm 30 minutes earlier based on the NWS forcast of",weatherAlert, "roads")
+        print("\033[1;32m \nVRS has changed your alarm 30 minutes earlier based on the NWS forcast of",weatherAlert, "roads")
         print("VRS will only allow your car to go 30MPH")
     elif weatherAlert == "Snow":
-        print("\nVRS has changed your alarm 15 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("\033[1;33m\nVRS has changed your alarm 15 minutes earlier based on the NWS forcast of",weatherAlert)
         print("VRS will only allow your car to go 50MPH")
     elif weatherAlert == "Rain":
-        print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("\033[1;34m\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
         print("VRS will only allow your car to go 60MPH")
     elif weatherAlert == "Windy":
-        print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("\033[1;35m\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
         print("VRS will only allow your car to go 70MPH")
     elif weatherAlert == "Foggy":
-        print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("\033[1;36m\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of",weatherAlert)
         print("VRS will only allow your car to go 60MPH")
     elif weatherAlert == "Storming":
-        print("\nVRS has changed your alarm 15 minutes earlier based on the NWS forcast of",weatherAlert)
+        print("\033[1;31m\nVRS has changed your alarm 15 minutes earlier based on the NWS forcast of",weatherAlert)
         print("VRS will only allow your car to go 50MPH")
     else:
-        print("\nThe weather today is,",weatherAlert,"let's Gooooo!")
+        print("\033[1;37m\nThe weather today is,",weatherAlert,"let's Gooooo!")
         print("VRS will only allow your car to go 85MPH")
 vRS()
 
